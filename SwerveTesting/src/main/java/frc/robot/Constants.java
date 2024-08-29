2f2493daa77acc69bd0d38cc6023613b92209394
Avi Sharma <asharmava1@gmail.com>
2024-08-29 15:03:32 -0700

    Swerve Config Halfway; Need to add documentation and finish Swerve.java along with pathplaner

--- SwerveTesting/src/main/java/frc/robot/Constants.java
+++ SwerveTesting/src/main/java/frc/robot/Constants.java
@@ -1,19 +1,142 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
+
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static final class Swerve {
+        public static final String swerveCanBus = "rio";
+
+        public static String[] modNames = { "FL ", "FR ", "BL ", "BR " };
+
+        
+
+        public static final int pigeonID = 13;
+
+        public static final double trackWidth = Units.inchesToMeters(21.73);
+        public static final double wheelBase = Units.inchesToMeters(21.73);
+        public static final double wheelDiameter = Units.inchesToMeters(4.0);
+        public static final double wheelCircumference = wheelDiameter * Math.PI;
+
+        public static final double openLoopRamp = 0.25;
+        public static final double closedLoopRamp = 0.0;
+
+        public static final double driveGearRatio = (6.75 / 1.0); // L1 - 8.14:1
+        public static final double angleGearRatio = 1 / ((14.0 / 50.0) * (10.0 / 60.0)); //21.43 
+
+        public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+            new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
+            new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+            new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+            new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+
+        /* Swerve Voltage Compensation */
+        public static final double voltageComp = 12.0;
+
+        /* Swerve Current Limiting */
+        public static final int angleContinuousCurrentLimit = 20;
+        public static final int driveContinuousCurrentLimit = 80;
+
+        /* Angle Motor PID Values */
+        public static final double angleKP = 0.01;
+        public static final double angleKI = 0.0;
+        public static final double angleKD = 0.0;
+        public static final double angleKFF = 0.0;
+
+        /* Drive Motor PID Values */
+        public static final double driveKP = 0.1;
+        public static final double driveKI = 0.0;
+        public static final double driveKD = 0.0;
+        public static final double driveKFF = 0.0;
+
+        /* Drive Motor Characterization Values */
+        public static final double driveKS = 0.667;
+        public static final double driveKV = 2.44;
+        public static final double driveKA = 0.27;
+
+        /* Drive Motor Conversion Factors */
+        public static final double driveConversionPositionFactor = (wheelDiameter * Math.PI) / driveGearRatio;
+        public static final double driveConversionVelocityFactor = driveConversionPositionFactor / 60.0;
+        public static final double angleConversionFactor = 360.0 / angleGearRatio;
+
+        /* Swerve Profiling Values */
+        public static final double maxSpeed = 4.5; // meters per second
+        public static final double maxAngularVelocity = 11.5;
+
+        /* Neutral Modes */
+        public static final IdleMode angleNeutralMode = IdleMode.kBrake;
+        public static final IdleMode driveNeutralMode = IdleMode.kBrake;
+
+        /* Motor Inverts */
+    public static final boolean driveInvert = false;
+    public static final boolean angleInvert = true;
+
+    /* Angle Encoder Invert */
+    public static final boolean canCoderInvert = false;
+
+        /* Module Specific Constants */
+        /* Front Left Module - Module 0 */
+        public static final class Mod0 { 
+            public static final int driveMotorID = 0;
+            public static final int angleMotorID = 1;
+            public static final int canCoderID = 0;
+            public static final String canBusID = swerveCanBus;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(33.39);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, canBusID, angleOffset);
+        }
+
+        /* Front Right Module - Module 1 */
+        public static final class Mod1 { 
+            public static final int driveMotorID = 18;
+            public static final int angleMotorID = 19;
+            public static final int canCoderID = 1;
+            public static final String canBusID = swerveCanBus;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-70.83);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, canBusID, angleOffset);
+        }
+        
+        /* Back Left Module - Module 2 */
+        public static final class Mod2 { 
+            public static final int driveMotorID = 8;
+            public static final int angleMotorID = 9;
+            public static final int canCoderID = 2;
+            public static final String canBusID = swerveCanBus;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(163.82);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, canBusID, angleOffset);
+        }
+
+        /* Back Right Module - Module 3 */
+        public static final class Mod3 { 
+            public static final int driveMotorID = 10;
+            public static final int angleMotorID = 11;
+            public static final int canCoderID = 3;
+            public static final String canBusID = swerveCanBus;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-25.13);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, canBusID, angleOffset);
+        }
+    }
+
+    public static final class AutoConstants {
+        public static final double kMaxSpeedMetersPerSecond = Swerve.maxSpeed / 4;
+        public static final double kMaxAngularSpeedRadiansPerSecond = Swerve.maxAngularVelocity / 10;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Math.PI / 4;
+        public static final double kPXController = 1.5;
+        public static final double kPYController = 1.5;
+        public static final double kPThetaController = 3;
+    }
+
+    public static final class OIConstants {
+        public static final int kDriverControllerPort = 0;
+        public static final double kDeadband = 0.05;
+    }
 }
---
+++ SwerveTesting/src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,152 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Swerve extends SubsystemBase {
+    private final Pigeon2 gyro;
+
+    private SwerveDriveOdometry swerveOdometry;
+    private SwerveModule[] mSwerveMods;
+
+    public Swerve() {
+        gyro = new Pigeon2(Swerve.pigeonID, Swerve.swerveCanBus);
+        gyro.configFactoryDefault();
+        zeroGyro();
+
+        mSwerveMods = new SwerveModule[] {
+            new SwerveModule(0, Constants.Swerve.Mod0.constants),
+            new SwerveModule(1, Constants.Swerve.Mod1.constants),
+            new SwerveModule(2, Constants.Swerve.Mod2.constants),
+            new SwerveModule(3, Constants.Swerve.Mod3.constants)
+        };
+
+        swerveOdometry = new SwerveDriveOdometry(Swerve.swerveKinematics, getYaw(), getModulePositions());
+
+        configurePathPlanner();
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+        SwerveModuleState[] swerveModuleStates =
+            Swerve.swerveKinematics.toSwerveModuleStates(
+                fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                                    translation.getX(), 
+                                    translation.getY(), 
+                                    rotation, 
+                                    getYaw()
+                                )
+                                : new ChassisSpeeds(
+                                    translation.getX(), 
+                                    translation.getY(), 
+                                    rotation)
+                                );
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Swerve.maxSpeed);
+
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+        }
+    }    
+
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Swerve.maxSpeed);
+        
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+        }
+    }    
+
+    public Pose2d getPose() {
+        return swerveOdometry.getPoseMeters();
+    }
+
+    public void resetOdometry(Pose2d pose) {
+        swerveOdometry.resetPosition(getYaw(), getModulePositions(), pose);
+    }
+
+    public SwerveModuleState[] getModuleStates() {
+        SwerveModuleState[] states = new SwerveModuleState[4];
+        for(SwerveModule mod : mSwerveMods){
+            states[mod.moduleNumber] = mod.getState();
+        }
+        return states;
+    }
+
+    public SwerveModulePosition[] getModulePositions() {
+        SwerveModulePosition[] positions = new SwerveModulePosition[4];
+        for(SwerveModule mod : mSwerveMods){
+            positions[mod.moduleNumber] = mod.getPosition();
+        }
+        return positions;
+    }
+
+    public void zeroGyro(){
+        gyro.setYaw(0);
+    }
+
+    public Rotation2d getYaw() {
+        return (Swerve.invertGyro) ? Rotation2d.fromDegrees(360 - gyro.getYaw().getValue()) : Rotation2d.fromDegrees(gyro.getYaw().getValue());
+    }
+
+    @Override
+    public void periodic(){
+        swerveOdometry.update(getYaw(), getModulePositions());  
+
+        for(SwerveModule mod : mSwerveMods){
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Integrated", mod.getPosition().angle.getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);    
+        }
+    }
+
+    private void configurePathPlanner() {
+        AutoBuilder.configureHolonomic(
+            this::getPose,
+            this::resetOdometry,
+            this::getRobotRelativeSpeeds,
+            this::driveRobotRelative,
+            new HolonomicPathFollowerConfig(
+                new PIDConstants(Swerve.driveKP, Swerve.driveKI, Swerve.driveKD),
+                new PIDConstants(Swerve.angleKP, Swerve.angleKI, Swerve.angleKD),
+                Swerve.maxSpeed,
+                Swerve.wheelBase / 2,
+                new ReplanningConfig()
+            ),
+            () -> {
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent()) {
+                    return alliance.get() == DriverStation.Alliance.Red;
+                }
+                return false;
+            },
+            this
+        );
+    }
+
+    private ChassisSpeeds getRobotRelativeSpeeds() {
+        return Swerve.swerveKinematics.toChassisSpeeds(getModuleStates());
+    }
+
+    private void driveRobotRelative(ChassisSpeeds speeds) {
+        drive(
+            new Translation2d(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond),
+            speeds.omegaRadiansPerSecond,
+            false,
+            false
+        );
+    }
+}
